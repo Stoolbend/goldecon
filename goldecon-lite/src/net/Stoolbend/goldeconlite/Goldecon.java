72f12830b7e689189a997320937e66e3fa5c21d5
Stoolbend <stoolbend@mc-server.co.uk>
2012-03-01 10:16:24 +0000

    Version 1.5a - Improved permission system readding support for PermissionsEx.

--- goldecon-lite/src/net/Stoolbend/goldeconlite/Goldecon.java
+++ goldecon-lite/src/net/Stoolbend/goldeconlite/Goldecon.java
@@ -12,6 +12,9 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import ru.tehkode.permissions.PermissionManager;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
+
 import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
 
@@ -19,7 +22,13 @@ public class Goldecon extends JavaPlugin{
 
 	Logger log;
 	PluginDescriptionFile info;
-	public static PermissionHandler permissions;
+	// Declare perm system variables
+	PermissionHandler perms3;
+	PermissionManager permsEx;
+	public static int permSystem;
+	// Display text thats editable
+	public static String ver = "1.5a";
+	public static String edition = ChatColor.GOLD + "[ge] ";
 	
 	public void onEnable()
 	  {
@@ -39,12 +48,12 @@ public class Goldecon extends JavaPlugin{
 		      if (cmd.getName().equalsIgnoreCase("ge"))
 		      {
 		    	if(!checkPerm(player, "goldecon.core.use")){
-		    		player.sendMessage(ChatColor.GOLD + "[ge] " + ChatColor.RED + "You dont have permission to do that, dave.");
+		    		player.sendMessage(edition + ChatColor.RED + "You dont have permission to do that, dave.");
 		    		return true;
 		    	}
 		     if(args.length == 0){
 		        player.sendMessage(ChatColor.GOLD + "<- goldecon-lite | Core Commands ->");
-		        player.sendMessage(ChatColor.RED + "| goldecon-lite by Stoolbend - Version 1.5+ |");
+		        player.sendMessage(ChatColor.RED + "| goldecon-lite by Stoolbend - Version " + ver + " |");
 		        player.sendMessage(ChatColor.RED + "| goldecon by boardinggamer & Kierrow |");
 		        player.sendMessage(ChatColor.AQUA + "/ge " + ChatColor.LIGHT_PURPLE + "pay <player name> <amount>" + ChatColor.GREEN + "- pay a player");
 //TODO Disabled see below		        player.sendMessage(ChatColor.AQUA + "/ge " + ChatColor.LIGHT_PURPLE + "stats " + ChatColor.GREEN + "- gets the goldecon stats");
@@ -64,20 +73,20 @@ public class Goldecon extends JavaPlugin{
 		                  player.sendMessage(ChatColor.DARK_PURPLE + "You paid " + args[1] + " " + amount + " gold.");
 		                  return true;
 		                } else {
-		                  player.sendMessage(ChatColor.GOLD + "[ge+] " + ChatColor.RED + "You don't have that much money with you.");
+		                  player.sendMessage(edition + ChatColor.RED + "You don't have that much money with you.");
 		                  return true;
 		                }
 		              } else {
-		                player.sendMessage(ChatColor.GOLD + "[ge+] " + ChatColor.RED + "You have no money with you.");
+		                player.sendMessage(edition + ChatColor.RED + "You have no money with you.");
 		                return true;
 		              }
 		            }
-		            else player.sendMessage(ChatColor.GOLD + "[ge+] " + ChatColor.RED + "Can not find " + args[1] + ".");
+		            else player.sendMessage(edition + ChatColor.RED + "Can not find " + args[1] + ".");
 		            return true;
 		          }
 		        }
 		        else {
-		          player.sendMessage(ChatColor.GOLD + "[ge+] " + ChatColor.RED + "/ge pay <player> <amount>");
+		          player.sendMessage(edition + ChatColor.RED + "/ge pay <player> <amount>");
 		          return true;
 		        }
 		      }
@@ -122,13 +131,13 @@ public class Goldecon extends JavaPlugin{
 		                total += stack.getAmount();
 		              }
 
-		              sender.sendMessage(ChatColor.GOLD + "[ge+] " + ChatColor.GOLD + "You have " + total + " gold with you.");
+		              sender.sendMessage(edition + ChatColor.GOLD + "You have " + total + " gold with you.");
 		              return true;
 		            }
 		          }
 		          else
 		          {
-		            player.sendMessage(ChatColor.GOLD + "[ge+] " + ChatColor.RED + "/ge < help | pay <player name> <amount> | stats [player name] >");
+		            player.sendMessage(edition + ChatColor.RED + "/ge < help | pay <player name> <amount> | stats [player name] >");
 		            return true;
 		          }
 		        }
@@ -136,32 +145,59 @@ public class Goldecon extends JavaPlugin{
 		
 		else
 		{
-		    sender.sendMessage(ChatColor.GOLD + "[ge+] " + "This is for player use only.");
+		    sender.sendMessage(edition + "This is for player use only.");
 			return true;
 		}
 		return true;
 	  }
-	private void setupPermissions() {
-		if (permissions != null) {
-			return;
-		}
+private int setupPermissions() {
 	    
-		Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+	    // Check for PermissionsEX - If found, use it.
+	    Plugin permsEXpl = this.getServer().getPluginManager().getPlugin("PermissionsEx");
 	    
-		if (permissionsPlugin == null) {
-			this.log.info(this.info.getName() + " Permission system not detected, defaulting to OP");
-			return;
-		}
-    
-		permissions = ((Permissions) permissionsPlugin).getHandler();
-		this.log.info(this.info.getName() + " Found and will use plugin "+((Permissions)permissionsPlugin).getDescription().getFullName());
+	    if (!(permsEXpl == null)) {
+		    permsEx = PermissionsEx.getPermissionManager();
+		    this.log.info(this.info.getName() + " Found and will use plugin "+ permsEXpl.getDescription().getFullName());
+	        return 1;
+	    }
+	    
+	    // Check for Permissions 3.x - If found, use it.
+	    Plugin perms3pl = this.getServer().getPluginManager().getPlugin("Permissions");
+	    
+	    if (!(perms3pl == null)) {
+	    	perms3 = ((Permissions) perms3pl).getHandler();
+		    this.log.info(this.info.getName() + " Found and will use plugin "+((Permissions)perms3pl).getDescription().getFullName());
+	        return 2;
+	    }
+	    
+	    else{
+	    // Nothing there. Go to Opmode
+        this.log.info(this.info.getName() + " 3rd Party permissions system not detected, defaulting to OP / SuperPerm mode.");
+        return 0;
+	    }
 	}
-	public boolean checkPerm(Player plyr, String node){
-		if(permissions.has(plyr, node) || plyr.hasPermission(node) || plyr.isOp()){
-			return true;
-		}
-		else{
-			return false;
-		}
+  public boolean checkPerm(Player plyr, String node){
+	if(permSystem == 1){
+	  	if(perms3.has(plyr, node) || plyr.isOp()){
+	  		return true;
+	  	}
 	}
+	else if(permSystem == 2){
+	  	if(permsEx.has(plyr, node) || plyr.isOp()){
+	  		return true;
+	  	}
+	}
+	else if(permSystem == 0){
+	  	if(plyr.hasPermission(node) || plyr.isOp()){
+	  		return true;
+	  	}
+	}
+  	else{
+        this.log.info(this.info.getName() + " The permission setup went wrong, Seek help! :(");
+        this.log.info(this.info.getName() + " Req Node: " + node);
+        this.log.info(this.info.getName() + " Perm System: " + permSystem);
+  		return false;
+  	}
+	return false;
+  }
 }
